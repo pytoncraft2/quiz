97f9204dee54d1310cf144972ebe0de07aed2c8f
pytoncraft2 <pythoncraft2@gmail.com>
2022-09-17 21:38:06 +0200

    🚧 page question

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const maxHeightImageQuestion = { maxHeightImageQuestion:200 }
--- src/scenes/Question.ts
+++ src/scenes/Question.ts
@@ -6,6 +6,8 @@
 import Phaser from "phaser";
 import PushOnClick from "../components/PushOnClick";
 /* START-USER-IMPORTS */
+import rectangle from "../utils";
+import { maxHeightImageQuestion } from "../constants";
 /* END-USER-IMPORTS */
 
 export default class Question extends Phaser.Scene {
@@ -76,6 +78,11 @@ export default class Question extends Phaser.Scene {
 
 	init(infos) {
 		console.log(infos);
+		
+        this.categoryIndexSelected = infos[2];
+        this.currentQuestionIndex = infos[3];
+        this.remainingLives = infos[4];
+        this.score = infos[5];
 	}
 
 	create() {
@@ -91,7 +98,128 @@ export default class Question extends Phaser.Scene {
 	      .on('pointerdown', function() {
 			this.scene.start('Fin');
 		}, this);
+
+        // this.marioWinSound = this.game.add.audio('mario_haha');
+        // this.marioLoseSound = this.game.add.audio('mario_lose');
+        this.rectCanvas = rectangle(this);
+
+        var data = this.cache.json.get('questions');
+        this.data = data;
+        this.totalLives = data.lives;
+
+        var imageQuestion = this.showImageQuestion(this.categoryIndexSelected,this.currentQuestionIndex);
+        this.livesGroups = this.showLives(this.remainingLives);
+        var questionItem = this.getQuestionItem(this.categoryIndexSelected,this.currentQuestionIndex);
+        this.showQuestion(questionItem,imageQuestion);
+        var totalQuestions = this.listQuestionsByCategory(this.categoryIndexSelected).length
+        this.showScore(this.score,totalQuestions);
+        this.showExitButton();
+
 	}
+
+    showQuestion(questionItem,imageQuestion){
+        var questionTitleElement = this.addQuestionTitle(questionItem.question,imageQuestion);
+        this.addButtonsChoice(questionItem.choices,questionItem.answer,questionTitleElement);
+    }
+
+    addQuestionTitle(textContent,imageQuestion){
+        var questionTitleElement = this.game.add.text(0,0,textContent, {
+            font: "24pt Audiowide", 
+            fill: "#000000", 
+            wordWrap: true,  
+            wordWrapWidth:800,
+            align: "left", 
+            backgroundColor: '#ffffff' 
+        });
+        questionTitleElement.alignTo(imageQuestion,Phaser.BOTTOM_CENTER);
+        return questionTitleElement;
+    }
+    addButtonsChoice(choicesText,answerIndex,questionTitleElement){
+        var groupButtons = this.game.add.group();
+        var previousGroup;
+        for(var index=0;index<choicesText.length;index++){
+            var isRightAnswer = (index===answerIndex);
+            var group = this.addChoiceGroup(choicesText[index],isRightAnswer);
+            if(previousGroup){
+                group.alignTo(previousGroup, Phaser.BOTTOM_LEFT, 0);
+            }
+            previousGroup = group;
+            groupButtons.add(group);
+        }
+        groupButtons.alignTo(questionTitleElement, Phaser.BOTTOM_CENTER, 0);
+    }
+    addChoiceGroup(title,isRightAnswer){
+        var button = this.game.add.button(0,0, 'button', this.onButtonChoiceClicked, {context:this,isRightAnswer:isRightAnswer}, 2, 1, 0);
+        button.scale.set(0.5);
+        var text = this.game.add.text(0,0,title, {font: "12pt Audiowide", fill: "#000000", wordWrap: false,  align: "left", backgroundColor: '#ffffff' });
+        text.alignTo(button, Phaser.RIGHT_CENTER, 0);
+        var group = this.game.add.group();
+        group.add(button);
+        group.add(text);
+        return group;
+    }
+    onButtonChoiceClicked(){
+        var context = this.context;
+        if(this.isRightAnswer){
+            context.score++;            
+            context.marioWinSound.play();
+        }else{
+            context.marioLoseSound.play();
+            context.remainingLives--;
+        }
+        if(context.remainingLives>0){
+            context.game.state.start('answer',true,false,context.categoryIndexSelected,context.currentQuestionIndex,this.isRightAnswer,context.remainingLives,context.score);
+        }else{
+            var isWin = false
+            context.game.state.start('endgame',true,false,isWin);
+        }
+    }
+    listQuestionsByCategory(categoryIndex){
+        return this.data.categories[categoryIndex].questions;
+    }
+    getQuestionItem(categoryIndex,questionIndex){
+        return this.listQuestionsByCategory(categoryIndex)[questionIndex];
+    }
+    showImageQuestion(categoryIndex,questionIndex){
+        var key = ['image_question',categoryIndex,questionIndex].join('_');
+        var image_question = this.add.image(0,0, key);
+        var scale = 1;
+        if(image_question.height > maxHeightImageQuestion){
+            scale = QuizGame.Constants.maxHeightImageQuestion/image_question.height;      
+        }
+        image_question.scale.set(scale);
+        image_question.alignIn(this.rectCanvas,Phaser.TOP_CENTER);
+        return image_question;
+    }
+    showLives(lives){
+        var group = this.game.add.group();
+        var previous;
+        for(var index=0;index<lives;index++){
+            var heart = this.game.add.sprite(0,0, 'heart');
+            if(previous){
+                heart.alignTo(previous, Phaser.RIGHT_CENTER, 0);
+            }
+            previous = heart;
+            group.add(heart);
+        }
+        group.alignIn(this.rectCanvas,Phaser.TOP_RIGHT)
+        return group;
+    }
+    showScore(score,total){
+        var style = { 
+            font: "20pt Audiowide", fill: "#7C00F8", wordWrap: false,  align: "right", backgroundColor: '#ffffff'
+        };
+        var textContent = 'Score : '+score+'/'+total;
+        var textEl = this.game.add.text(0,0,textContent, style);
+        textEl.alignTo(this.livesGroups,Phaser.BOTTOM_RIGHT);
+    }
+    showExitButton(){
+        var button = this.game.add.button(0,0, 'exitButton', this.onButtonExitClicked, this, 2, 1, 0);        
+        button.alignIn(this.rectCanvas,Phaser.BOTTOM_RIGHT);
+    }
+    onButtonExitClicked(){
+        this.scene.start('intro');
+    }
 
 	/* END-USER-CODE */
 }
